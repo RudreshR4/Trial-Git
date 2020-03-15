5a37d8034e9985ac62aa36f6be6780ce81d1babb
RudreshR4 <58745210+RudreshR4@users.noreply.github.com>
2020-03-15 18:13:01 +0530

    Add files via upload
    Implementation of LinkedList as an ArrayList-commited

---
+++ TrailDemoTestOne.java
@@ -0,0 +1,22 @@
+package linkedListEx;
+
+//http://www.eecs.qmul.ac.uk/~mmh/DCS128/notes/altArrayLists1.html
+//http://www.eecs.qmul.ac.uk/~mmh/DCS128/notes/code/linkedlists/MyArrayList5.java
+
+public class TrailDemoTestOne {
+public static void main(String[] args) {
+DemoTestOne<Integer> one = new DemoTestOne<>();
+one.add(2);
+one.add(4);
+one.add(7);
+one.add(8);
+one.add(4);
+System.out.println("List size:"+one.size());
+System.out.println("Get():"+one.get(1));
+System.out.println("indexOf:"+one.indexOf(7));
+one.set(4,10);
+System.out.println("Afer set():"+one.get(4));
+one.showList();
+}
+}
+
